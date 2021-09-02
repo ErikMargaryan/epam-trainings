b39453320206319b5e63f09aedb5ccb7da1fcada
Asus <koryun1211@gmail.com>
2021-09-02 11:20:45 +0400

    2nd homework done!

--- src/com/epam/javatrainings/array/ArrayUtil.java
+++ src/com/epam/javatrainings/array/ArrayUtil.java
@@ -1,105 +1,106 @@
-package com.epam.javatrainings.array;
+import java.util.
+class Order {
+    private static int ord_num=11000;
+    private static int custom_num=1500;
+    Pizza P;
+    Order()
+    {
+        set_order();
+        ord_num+=1;
+        custom_num+=1;
+    }
+    public void set_order()
+    {
+        char ch;
+        System.out.print("Hi. What would you like to order? ");
+        Scanner S=new Scanner(System.in);
 
-public class ArrayUtil {
+        do {
+            P=new Pizza();
+            System.out.print("Is that all or you will something more?(y/n): ");
+            ch=S.nextChar();
+        }while(ch!='n');
+    }
 
-    /**
-     * Sorts the array elements by odd and even number
-     * such that if first number is odd then second must be even number
-     * this means that it will be found the next even number and swap with the second element
-     * if the second element is odd. And so on for the next elements of the array
-     */
-    public static void sortByOddEven(int[] array) {
-        for (int i = 0; i < array.length - 2; i++) {
-            int j = i + 1;
-            while ((array[i] + array[j]) % 2 == 0) {
-                j++;
-                if (j >= array.length) {
-                    return;
-                }
-            }
+    int getCustom_num()
+    {return custom_num;}
+    int getOrd_num()
+    {return ord_num;}
 
-            swap(array, i + 1, j);
+}
+
+class Pizza {
+    private String pizza_name;
+    private String ingrid_name[]={"tomato","cheese","salami","backon","garlic","corn","pepperoni","olives"};
+    private double tomato,cheese,salami,backon,garlic,corn,pepperoni,olives;
+    private double ingrid[]={tomato=1,cheese=1,salami=1.5,backon=1.2,garlic=0.3,corn=0.7,pepperoni=0.6,olives=0.5}
+    private int quantity;
+    private String pizza_type;
+    private float item_price=1;
+
+    Pizza() {
+        P_type();
+        this.P_name();
+        this.choose_ingridients();
+    }
+    public void P_type()
+    {
+        char ch;
+        Scanner S=new Scanner(System.in);
+        System.out.print("select pizza type(ordinar(o)/calzone(c): ");
+        ch=S.nextChar();
+        if(ch=='o')
+            pizza_type="ordinar";
+        else
+        if(ch=='c')
+        {
+            pizza_type="Calzone";
+            item_price += 0.5;
         }
     }
-
-    private static void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+    public void P_name()
+    {
+        Scanner S=new Scanner(System.in);
+        System.out.print("enter the pizza name: ");
+        this.pizza_name=S.nextLine();
+    }
+    public void choose_ingridients()
+    {
+        char ch;
+        int k=0;
+        Scanner s = new Scanner(System.in);
+        do {
+            System.out.print("add? "+ingrid_name[k]+"y/n");
+            ch = s.nextChar();
+            if(ch=='y')
+                this.item_price+=this.ingrid[k];
+            k++;
+        }while(k< ingrid_name.length);
+        Scanner S=new Scanner(System.in);
+        this.pizza_name=S.nextLine();
+        System.out.print("enter the quantity: ");
+    }
+    public float pizza_price()
+    {
+        int q;
+        Scanner s = new Scanner(System.in);
+        System.out.print("enter quantity: ");
+        q = s.nextInt();
+        return this.item_price*q;
     }
 
-    /**
-     * Return maximum element from given array
-     */
-    public static int getMaximum(int[] array) {
-        int max = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
-            }
-        }
-        return max;
+}
+public class Order_palm{
+    public static void main(String args[]) {
+        System.out.print("wellcome to Palmet: ");
+        Scanner s = new Scanner(System.in);
+        Order ord;
+        char ch;
+        do
+        {
+            ord=new Order();
+            System.out.print("NEXT ORDER(y/n): ");
+            ch = s.nextChar();
+        }while(ch!='n');
     }
-
-    /**
-     * Return minimum element from given array
-     */
-    public static int getMinimum(int[] array) {
-        int min = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < min) {
-                min = array[i];
-            }
-        }
-        return min;
-
-    }
-
-    /**
-     * Calculates the sum of specified array  elements
-     *
-     * @param array specified int array
-     * @return the sum of the specified array elements
-     */
-    public static int getSum(int[] array) {
-        int sum = 0;
-        for (int j : array) {
-            sum += j;
-        }
-        return sum;
-    }
-
-    /**
-     * Revers given array
-     * Example:
-     * Given array [2,5,7,3,2,6]
-     * Expected array [6,2,3,7,5,2]
-     */
-    public static int[] reverse(int[] array) {
-        int i = 0;
-        int j = array.length - 1;
-
-        while (i < j) {
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            i++;
-            j--;
-        }
-
-        return array;
-    }
-
-    /**
-     * Prints all elements in an array.
-     *
-     * @param array the array to print
-     */
-    public static void print(int[] array) {
-        for (int j : array) {
-            System.out.print(j + " ");
-        }
-    }
-
-
 }
