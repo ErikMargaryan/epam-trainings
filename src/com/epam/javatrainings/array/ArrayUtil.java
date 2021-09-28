f0ee4c0bec2ac2821b69e666dcc4c285b011997e
Asus <koryun1211@gmail.com>
2021-09-28 12:14:06 +0400

    some corrections done(28/09/2021)

--- src/com/epam/javatrainings/array/ArrayUtil.java
+++ src/com/epam/javatrainings/array/ArrayUtil.java
@@ -1,21 +1,22 @@
-import java.util.
+import java.util.Scanner;
 class Order {
     private int ord_num;
     private int custom_num;
-    private static int stat_ord_num =11000;
-    private static int stat_custom_num=1500;
+    int ord_num =11000;
+    private static int custom_num=1500;
+    Random ord_rand,custom_rand;
     Pizza P;
     Pizza p_arr[];
     Order()
     {
-        stat_custom_num++;
-        stat_ord_num++;
+        ord_rand = new Random();
+        custom_rand = new Random();
+        custom_num=10000*custom_rand.nextDouble();
+        ord_num=100000*ord_rand.nextDouble();
         set_order();
     }
     public void set_order()
     {
-        ord_num=stat_ord_num;
-        custom_num=stat_custom_num;
         char ch;
         System.out.print("Hi. What would you like to order? ");
         Scanner S=new Scanner(System.in);
@@ -23,7 +24,7 @@ class Order {
         do {
             P=new Pizza();
             System.out.print("Is that all or you will something more?(y/n): ");
-            ch=S.nextChar();
+            ch=S.next().charAt(0);
             p_arr[k]=P;
             k++;
         }while(ch!='n');
@@ -31,21 +32,21 @@ class Order {
 
     public void printCheck()
     {
-        System.out.print("Order: " + ord_num);
-        System.out.print("Client: " + custom_num);
+        System.out.println("Order: " + ord_num);
+        System.out.println("Client: " + custom_num);
         for(inti=0;i<p_arr.length;i++) {
-            System.out.print("Name: " + p_arr[i].P_name());
-            System.out.print("................................ ");
-            System.out.print("Pizza baze(" + p_arr[i].P_type() + ")" + p_arr[i].getPizza_type_price() + '$');
+            System.out.println("Name: " + p_arr[i].P_name());
+            System.out.println("................................ ");
+            System.out.println("Pizza baze(" + p_arr[i].P_type() + ")" + p_arr[i].getPizza_type_price() + '$');
             p_arr[i].print_ingrid();
-            System.out.print("Qmmount: "+p_arr[i].getAmmount());
-            System.out.print("Ammount: "+p_arr[i].getQuantity());
-            System.out.print("................................ ");
+            System.out.println("Qmmount: "+p_arr[i].getAmmount());
+            System.out.println("Ammount: "+p_arr[i].getQuantity());
+            System.out.println("................................ ");
         }
         double Ammount=0;
         for(int i=0;i<p_arr.length;i++)
             temp_Ammount+=p_arr[i];
-        System.out.print("Total ammount: "+temp_Ammount);
+        System.out.println("Total ammount: "+temp_Ammount);
     }
 
     int getCustom_num()
@@ -59,7 +60,7 @@ class Pizza {
     private String pizza_name;
     private String ingrid_name[]={"tomato","cheese","salami","backon","garlic","corn","pepperoni","olives"};
     private double tomato,cheese,salami,backon,garlic,corn,pepperoni,olives;
-    private double ingrid[]={tomato=1,cheese=1,salami=1.5,backon=1.2,garlic=0.3,corn=0.7,pepperoni=0.6,olives=0.5}
+    private double ingrid[]={tomato=1,cheese=1,salami=1.5,backon=1.2,garlic=0.3,corn=0.7,pepperoni=0.6,olives=0.5};
     private int quantity;
     private String pizza_type;
     private float item_price=1;
@@ -81,7 +82,7 @@ class Pizza {
         String s;
         Scanner S=new Scanner(System.in);
         System.out.print("select pizza type(ordinar(o)/calzone(c): ");
-        ch=S.nextChar();
+        ch=S.next().charAt(0);
         if(ch=='o')
             pizza_type="ordinar";
         else
@@ -97,7 +98,7 @@ class Pizza {
     public String P_name()
     {
         Scanner S=new Scanner(System.in);
-        System.out.print("enter the pizza name: ");
+        System.out.println("enter the pizza name: ");
         String s=S.nextLine();
         pizza_name=s;
         return s;
@@ -110,7 +111,7 @@ class Pizza {
         Scanner s = new Scanner(System.in);
         do {
             System.out.print("add? "+ingrid_name[k]+"y/n");
-            ch = s.nextChar();
+            ch = s.next().charAt(0);;
             if(ch=='y') {
                 this.item_price += this.ingrid[k];
                 ingrid_env[index++]=k;
@@ -162,7 +163,7 @@ class Pizza {
                 ord=new Order();
                 ord_arr[ord_arr_index++]=ord;
                 System.out.print("NEXT ORDER(y/n): ");
-                ch = s.nextChar();
+                ch = s.next().charAt(0);
             }while(ch!='n');
 
             for(int i=0;i<ord_arr_index;i++)
