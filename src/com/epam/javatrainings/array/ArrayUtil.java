b19ffac1895659ad174170cdae622385da4d28db
Asus <koryun1211@gmail.com>
2021-09-28 14:34:52 +0400

    5th homework done

--- src/com/epam/javatrainings/array/ArrayUtil.java
+++ src/com/epam/javatrainings/array/ArrayUtil.java
@@ -1,105 +1,45 @@
-package com.epam.javatrainings.array;
-
-public class ArrayUtil {
-
-    /**
-     * Sorts the array elements by odd and even number
-     * such that if first number is odd then second must be even number
-     * this means that it will be found the next even number and swap with the second element
-     * if the second element is odd. And so on for the next elements of the array
-     */
-    public static void sortByOddEven(int[] array) {
-        for (int i = 0; i < array.length - 2; i++) {
-            int j = i + 1;
-            while ((array[i] + array[j]) % 2 == 0) {
-                j++;
-                if (j >= array.length) {
-                    return;
-                }
-            }
-
-            swap(array, i + 1, j);
-        }
+import java.util.Scanner;
+class StopWatch implements Runnable
+{
+    Thread t;
+    private int minutes;
+    private int seconds;
+    StopWatch(int min,int sec)
+    {
+        minutes =min;
+        seconds=sec;
+        t=new Thread(this," Demothread ");
+        t.start();
     }
 
-    private static void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-
-    /**
-     * Return maximum element from given array
-     */
-    public static int getMaximum(int[] array) {
-        int max = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
+    public void run()
+    {
+        int total=minutes*60+seconds;
+        while(total >0)
+        {
+            total-=1;
+            minutes=total/60;
+            seconds=total%60;
+            System.out.println(minutes+":"+seconds);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
-        return max;
     }
+}
 
-    /**
-     * Return minimum element from given array
-     */
-    public static int getMinimum(int[] array) {
-        int min = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < min) {
-                min = array[i];
-            }
-        }
-        return min;
-
+class ThreadDemo
+{
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        int Mins;
+        int Seconds;
+        System.out.println("please enter the minutes than seconds: ");
+        Mins=scan.nextInt();
+        Seconds=scan.nextInt();
+        StopWatch ob=new StopWatch(Mins,Seconds);
     }
-
-    /**
-     * Calculates the sum of specified array  elements
-     *
-     * @param array specified int array
-     * @return the sum of the specified array elements
-     */
-    public static int getSum(int[] array) {
-        int sum = 0;
-        for (int j : array) {
-            sum += j;
-        }
-        return sum;
-    }
-
-    /**
-     * Revers given array
-     * Example:
-     * Given array [2,5,7,3,2,6]
-     * Expected array [6,2,3,7,5,2]
-     */
-    public static int[] reverse(int[] array) {
-        int i = 0;
-        int j = array.length - 1;
-
-        while (i < j) {
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            i++;
-            j--;
-        }
-
-        return array;
-    }
-
-    /**
-     * Prints all elements in an array.
-     *
-     * @param array the array to print
-     */
-    public static void print(int[] array) {
-        for (int j : array) {
-            System.out.print(j + " ");
-        }
-    }
-
-
 }
